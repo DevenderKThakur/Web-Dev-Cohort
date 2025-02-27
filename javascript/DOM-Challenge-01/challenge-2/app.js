a5a67fe32ea8fa0040873f0f8aea522d2cc0845a
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2025-02-27 19:05:20 +0530

    Update app.js

--- javascript/DOM-Challenge-01/challenge-2/app.js
+++ javascript/DOM-Challenge-01/challenge-2/app.js
@@ -1,3 +1,15 @@
-/**
- * Write your challenge solution here
- */
+const heading = document.getElementById("mainHeading");
+
+const allButtons = Array.from(
+  document.querySelector(".color-buttons").children
+);
+
+allButtons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.textContent !== "Reset") {
+      heading.style.color = btn.textContent.toLowerCase();
+    } else {
+      heading.style.color = "black";
+    }
+  });
+});
