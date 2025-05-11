caced45833309659b278aa33a4415c363b2e7dfe
DevenderKThakur <tdevender422@gmail.com>
2025-05-11 20:33:22 +0530

    Added cors dotenv

--- fullStack/index.js
+++ fullStack/index.js
@@ -1,11 +1,37 @@
 import express from "express";
 
+import dotenv from "dotenv";
+
+import cors from "cors";
+
+dotenv.config();
+
 const app = express();
 
-const port = 3000;
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Cohort!");
+});
+
+app.get("/devender", (req, res) => {
+  res.send({
+    username: "username",
+    age: 22,
+  });
 });
 
 app.listen(port, () => console.log("Server is listening at port", port));
